9a3d36237734641afef4a2b5760e1e21ece99f6b
Dylan Jaipersaud <dyjaipersaud@gmail.com>
2022-12-01 17:38:03 -0500

    Added LoginPage and AppBar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app-bar
+      absolute
+      color="#050A30"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+      class="app-bar"
+    >
+      <v-spacer></v-spacer>
+      <v-btn color="#7EC8E3" outlined> Home </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="#7EC8E3" outlined> Calendar </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="#7EC8E3" outlined> Account </v-btn>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "AppBar",
+
+}
+</script>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="main-con">
+    <div class="left-con">
+      <div class="welcome-con">
+        <h1><strong>Welcome to TrainMe!</strong></h1>
+        <br />
+        <h2><i>Ready to get into shape?</i></h2>
+        <br /><br /><br />
+        <div class="join-con">
+          <h4>Join TrainMe now!</h4>
+          <v-btn color="#050A30" outlined>SIGN UP</v-btn>
+        </div>
+      </div>
+    </div>
+
+    <div class="right-con">
+      <div style="height: 100%">
+        <h2>Sign in to your Account</h2>
+        <br />
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          style="display: flex; flex-direction: column"
+        >
+          <v-text-field
+            v-model="email"
+            prepend-icon="mdi-email"
+            style="padding: 10px"
+            color="#050A30"
+            label="E-mail"
+            :rules="emailRules"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            prepend-icon="mdi-lock"
+            color="#050A30"
+            style="padding: 10px"
+            label="Password"
+            required
+            :rules="passwordRules"
+            type="password"
+          ></v-text-field>
+          <v-btn @click="validate" width="50%" outlined color="#050A30" style="align-self: center;">Submit</v-btn>
+        </v-form>
+        <br />
+        <br />
+        <h4>
+          <i>Forgot your password? </i> 
+          <v-btn text> Click here!</v-btn>
+        </h4>
+        <!-- <v-spacer></v-spacer>
+        <div>Pic</div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    email: null,
+    password: null,
+
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    passwordRules: [(v) => !!v || "Password is required"],
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        alert("Clicked Logged in");
+      } else {
+        alert("Login failed, please try again.");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main-con {
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.welcome-con {
+  padding: 100px;
+  border-radius: 25px;
+  background-color: whitesmoke;
+  box-shadow: -11px 11px 30px #7ec8e3;
+}
+.join-con {
+  display: grid;
+  width: 100%;
+  justify-items: center;
+  align-content: center;
+}
+.left-con {
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  justify-content: space-evenly;
+  height: 100%;
+  width: 60%;
+  background: url(https://verilymag.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTQ5NDQ1Njk3MjE5ODY0MjY1/fitnessroutines.png);
+}
+.right-con {
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  height: 100%;
+  width: 40%;
+  background-color: whitesmoke;
+
+  box-shadow: inset 11px 11px 5px #dadada;
+}
+</style>
